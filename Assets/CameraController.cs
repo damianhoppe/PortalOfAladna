4e23f66643243b0f563adaa78db3fc3ba626838f
eNesPL <enescraft@gmail.com>
2021-03-05 15:24:18 +0100

    Siatka i Kamera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int Boundary = 50;
+    [SerializeField]
+    private int speed = 20;
+ 
+    private int theScreenWidth;
+    private int theScreenHeight;
+
+    [SerializeField]
+    private int sprint = 0;
+    [SerializeField]
+    private float minZoom = 10f;
+    [SerializeField]
+    private float maxZoom = 50f;
+    [SerializeField]
+    private float zoomSpeed = 10f;
+    [SerializeField]
+    private float currentZoom = 10f;
+    [SerializeField]
+    Camera camera = new Camera();
+    void Start()
+    {
+        camera = Camera.main;
+        theScreenWidth = Screen.width;
+        theScreenHeight = Screen.height;
+    }
+    void Update()
+    {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                sprint = 30;
+            }
+            else
+            {
+                sprint = 1;
+            }
+
+            if (Input.mousePosition.x > theScreenWidth - Boundary)
+            {
+                transform.position += new Vector3((sprint + speed) * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.mousePosition.x < 0 + Boundary)
+            {
+
+                transform.position -= new Vector3((sprint + speed) * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.mousePosition.y > theScreenHeight - Boundary)
+            {
+                transform.position += new Vector3(0, (sprint + speed) * Time.deltaTime, 0);
+            }
+
+            if (Input.mousePosition.y < 0 + Boundary)
+            {
+                transform.position -= new Vector3(0, (sprint + speed) * Time.deltaTime, 0);
+            }
+
+
+            currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+            Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+            currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+            camera.orthographicSize = currentZoom;
+
+    }
+
+}
---
+++ Assets/GridManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Grid grid;
+    private void Start()
+    {
+        grid = new Grid(10, 10, 10f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.setValue(UtilsClass.GetMouseWorldPosition(),grid.getValue(UtilsClass.GetMouseWorldPosition())+1);
+        }
+    }
+}
