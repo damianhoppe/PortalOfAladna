1dbf0676a4a2ec3c34257378fc5a90abf2a720c1
eNesPL <enescraft@gmail.com>
2021-03-18 00:00:25 +0100

    pathfinder quickfix

---
+++ Assets/Tymczasowy/DayNightController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePathfinding()
+    {
+        AstarPath.active.Scan(); //temp
+    }
+
+    public static void ScanPathfinding()
+    {
+        AstarPath.active.Scan();
+    }
+    
+    void BlockBuilding()
+    {
+
+    }
+    void SpawnMonster()
+    {
+
+    }
+    public void BuildingDestroyed()
+    {
+        UpdatePathfinding();
+    }
+    void NightTime()
+    {
+        ScanPathfinding();
+        BlockBuilding();
+        SpawnMonster();
+    }
+}
---
+++ Assets/Tymczasowy/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using Pathfinding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AIPath ai;
+    GameObject portal;
+    AIDestinationSetter destination;
+    [SerializeField]
+    GameObject[] Targets;
+    void Start()
+    {
+        destination = GetComponent<AIDestinationSetter>();
+        portal = GameObject.FindWithTag("Portal");
+        ai = this.GetComponent<AIPath>();
+        ai.canMove = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ai.canMove = true;
+            destination.target = TargetSelection(Targets);
+            DayNightController.ScanPathfinding();
+        }
+    }
+
+    Transform TargetSelection(GameObject[] Targets)
+    {
+
+        /*List<float> aggro = new List<float>();
+        
+        int min=0;
+        for (int i=0;i<Targets.Length;i++)
+        {
+            destination.target = TargetSelection(Targets);
+            aggro.Add(ai.remainingDistance - Targets[i].GetComponent<Building>().Danger);
+            Debug.Log("Danger: " + Targets[i].GetComponent<Building>().Danger);
+            min = Array.IndexOf(aggro.ToArray(),aggro.Min());
+            
+        }
+
+                return Targets[min].transform;*/
+        ;
+        return GameObject.Find(Targets[0].name).transform;
+    }
+}
