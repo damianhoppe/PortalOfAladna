228623a78bd9658b3d7bc1102498aa84a08911a4
MKowalski11 <gasienica98@gmail.com>
2021-03-17 23:05:08 +0100

    Pliki + klasa bazowa jednostki
    Klasa bazowa jednostki gracza z atrybutami i przewydywanymi metodami

---
+++ Assets/TroopTypes.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopTypes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public class Cost
+    {
+        private int Gold { get; set; }
+        private int Human { get; set; }
+        private int Wood { get; set; }
+        private int Stone { get; set; }
+        private int Metal { get; set; }
+        private int Crystal { get; set; }
+        public Cost(int g, int h, int w, int s, int m, int c)
+        {
+            this.Gold = g;
+            this.Human = h;
+            this.Wood = w;
+            this.Stone = s;
+            this.Metal = m;
+            this.Crystal = c;
+        }
+    }
+    public class Default
+    {
+        // Domyslne wartosci dla HP/MP/Armor
+        private double MaxHitpoints { get; set; } = 100.0;
+        private double MaxEnergy { get; set; } = 0.0;
+        private double MaxArmor { get; set; } = 0.0; //Stala wartosc
+        private double MaxBlock { get; set; } = 0.0; //Procentowa
+
+        // Zmienne na odpornosci, jezeli beda
+        //private double FireResistance { get; set; } = 0.0; //%
+        //private double FireProtection { get; set; } = 0.0; //stala wartosc
+
+        // Zmienne dla siatki/pola na ktorym stoja
+        private int[] Position { get; set; } = new int[] { 0, 0 };
+        private double DefaultDanger { get; set; } = 0.0;
+        private double DefaultValue { get; set; } = 0.0;
+        private double DefaultObstacle { get; set; } = 0.0;
+        private double SightRange { get; set; } = 1.0;
+        private int[,] InteractionRange { get; set; } = new int[,] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+        // Zmienne ekonomiczne
+        private int UnitGoldCost { get; set; } = 100;
+        private int UnitHumanCost { get; set; } = 1;
+        private int UnitWoodCost { get; set; } = 0;
+        private int UnitStoneCost { get; set; } = 0;
+        private int UnitMetalCost { get; set; } = 0;
+        private int UnitCrystalCost { get; set; } = 0;
+        private int UnitBuildTime { get; set; } = 0;
+        private double UnitSellRefund { get; set; } = 100.0;
+        // Obiekt kosztu
+
+        public Cost UnitCost;
+
+        // Zmienne wizualne i ruch
+        private string UnitName { get; set; } = "Human";
+        private string UnitDescription { get; set; } = "I'm just useless human.";
+        private double Rotation { get; set; } = 0.0;
+        private double MaxSpeed { get; set; } = 1.0;
+
+        // Opcje dodatkowe
+        private bool CanUseGates { get; set; } = false;
+        private bool CanSwim { get; set; } = false;
+        private bool CanFly { get; set; } = false;
+        private bool IsFireproof { get; set; } = false;
+
+        private bool IsDead { get; set; } = false;
+        private bool IsAlive { get; set; } = true;
+
+        private bool IsFriendly { get; set; } = true;
+        private bool IsUseful { get; set; } = false;
+        private bool IsHostile { get; set; } = false;
+
+        // Statystyki obecne, najbardziej podstawowe
+        private double CurrentHitpoints { get; set; }
+        private double CurrentEnergy { get; set; }
+
+        // Statystyki obecne, dodatkowe (modyfikatory/negatywne/pozytywne statusy)
+        //private bool MaxArmor { get; set; }
+        //private bool MaxBlcok { get; set; }
+
+        public Default()
+        {
+            this.CurrentHitpoints = MaxHitpoints;
+            this.CurrentEnergy = CurrentHitpoints;
+            this.UnitCost = new Cost(this.UnitGoldCost, this.UnitHumanCost, this.UnitWoodCost, this.UnitStoneCost, this.UnitMetalCost, this.UnitCrystalCost);
+        }
+        //Default(UpgradesObject){}
+        public bool OnSpawn(int[] SpawnpointXY)
+        {
+            //if(Grid.CheckOccupied(SpawnpointXY)) return false;
+            //if(Economy.CheckCanAfford(this.UnitCost)=!true) return false;
+            //Economy.BuyUnit(this.UnitCost);
+            this.Position[0] = SpawnpointXY[0];
+            this.Position[1] = SpawnpointXY[1];
+            return true;
+        }
+        public void OnHitpointsChange(double Hitpoints)
+        {
+            this.CurrentHitpoints -= Hitpoints;
+            if (this.CurrentHitpoints > this.MaxHitpoints) this.CurrentHitpoints = this.MaxHitpoints;
+            if (this.CurrentHitpoints <= 0.0)
+            {
+                this.IsAlive = false;
+                this.IsDead = false;
+                this.OnUnitDeath();
+            }
+            return;
+        }
+        public bool OnEnergyChange(double Energy)
+        {
+            //bool - czy udalo sie zuzyc energie
+            if (this.CurrentEnergy >= Energy)
+            {
+                this.CurrentEnergy -= Energy;
+                if (this.CurrentEnergy > this.MaxEnergy) this.CurrentEnergy = this.MaxEnergy;
+                return true;
+            }
+            else return false;
+            
+        }
+        public void OnUnitDeath() {
+            //Co sie dzieje jak zgnije
+            //Animacja smierci?
+            return;
+        }
+        public void OnSell()
+        {
+            this.UnitSellRefund = this.CurrentHitpoints / this.MaxHitpoints;
+            if (this.UnitSellRefund > 1.0) UnitSellRefund = 1.0;
+            //Wywolanie funkcji zwracajacej koszt wraz z %-zwrotu
+            //Economy.Refund(this.Cost,this.UnitSellRefund);
+            return;
+        }
+        public void OnMove() 
+        {
+            //Tutaj funkcja do przesuwania jednostki, z warunkami czy mozna to zrobic?
+            return;
+        }
+        public void OnUpgrade()
+        {
+            //Co sie dzieje jak sie ulepsza?
+            return;
+        }
+    }
+    public class Peasant : Default
+    {
+
+    }
+    public class Warrior : Default
+    {
+
+    }
+    public class Archer : Default
+    {
+
+    }
+    public class Tank : Default
+    {
+
+    }
+    public class Scout : Default
+    {
+
+    }
+}
