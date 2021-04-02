b26388ac8bfbf2a609f2f625bd742506e913c0b7
MKowalski11 <gasienica98@gmail.com>
2021-04-02 02:44:12 +0200

    EC Basic Functionality
    Basic functionality for Economy Controller

--- Assets/GameEngine/EconomyController.cs
+++ Assets/GameEngine/EconomyController.cs
@@ -15,8 +15,25 @@ public class EconomyController : MonoBehaviour
     {
         
     }
+    
+    public DataStructures.Cost PlayerResources = new DataStructures.Cost();
+
     public bool CanAffordTEST(DataStructures.Cost koszt)
     {
+        
         return true;
+    }
+    public bool CanAfford(DataStructures.Cost koszt)
+    {
+        if (koszt <= PlayerResources) return true;
+        else return false;
+    }
+    public void ResourcesSpent(DataStructures.Cost koszt)
+    {
+        PlayerResources -= koszt;
+    }
+    public void ResourcesGained (DataStructure.Cost zysk)
+    {
+        PlayerResources -= zysk;
     }
 }
