6381335d3923900da8feb7cb8c29d583391f9a59
MKowalski11 <gasienica98@gmail.com>
2021-04-03 23:14:46 +0200

    Error, rollback
    Sie wyjebao

--- Assets/GameEngine/ObjectsData/Buildings/Lookout.cs
+++ Assets/GameEngine/ObjectsData/Buildings/Lookout.cs
@@ -15,14 +15,14 @@ public class Lookout : DefaultBuilding
     {
         base.Update();
     }
-
+    /*
     public override string ObjectName { get; protected set; } = "Lookout";
     public override string ObjectDescription { get; protected set; } = "This is a lookout, with high view range.";
     public override string ObjectType { get; protected set; } = "Building";
     public override int ObjectTypeID { get; protected set; } = 1;
     public override string ObjectSubtype { get; protected set; } = "Default Lookout";
     public override int ObjectSubtypeID { get; protected set; } = 2;
-    
+   
     public override bool IsMilitary { get; protected set; } = true;
     public override bool IsCivilian { get; protected set; } = false;
 
@@ -40,6 +40,35 @@ public class Lookout : DefaultBuilding
     public override DataStructures.Cost BaseCost { get; protected set; } = new DataStructures.Cost(50.0f, 15.0f, 5.0f, 0.0f, 0.0f, 0.0f);
     public override float EnergyToBuild { get; protected set; } = 10.0f;
     public override int RequiredHumans { get; protected set; } = 5;
+    */
+    public Lookout()
+    {
+        ObjectName = "Lookout";
+        ObjectDescription = "This is a lookout, with high view range.";
+        ObjectType = "Building";
+        ObjectTypeID = 1;
+        ObjectSubtype = "Default Lookout";
+        ObjectSubtypeID = 2;
+
+        IsMilitary = true;
+        IsCivilian = false;
+
+        Armor = 1.0f;
+        Protection = 5.0f;
+
+        MaxHitpoints = 100.0f;
+        SightRange = 5.0f;
+        ActiveAtNight = true;
+
+        PositionValue = 1.0f;
+        PositionObstacle = 1.0f;
+        PositionDanger = 2.5f;
+        
+        BaseCost = new DataStructures.Cost(50.0f, 15.0f, 5.0f, 0.0f, 0.0f, 0.0f);
+        EnergyToBuild = 10.0f;
+        RequiredHumans = 5;
+        
+    }
 
     public override bool onCreate()
     {
