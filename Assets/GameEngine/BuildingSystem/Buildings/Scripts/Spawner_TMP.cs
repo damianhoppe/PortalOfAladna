343052c9347a62550db046d40166b400939bc100
TomaszKanzner <80108529+TomaszKanzner@users.noreply.github.com>
2021-04-12 11:25:26 +0200

    System Spawnu podstawa
    - ma jakiś problem z targetem i po postawieniu spawnera i wspawnieniu enemy freezuje

--- Assets/GameEngine/BuildingSystem/Buildings/Scripts/Spawner_TMP.cs
+++ Assets/GameEngine/BuildingSystem/Buildings/Scripts/Spawner_TMP.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class Spawner_TMP : Building
 {
-    float HP;
+    //public float timeBetweenWaves = 10f;
+    //private float countdown = 2f;
+    //private int waveInWave = 1; //nalicza fale przeciwników w fali
+    //private int wave = 2; //nalicza fale przeciwników 
+
     [SerializeField]
     GameObject[] enemyArray;
     protected override void Start()
     {
         base.Start();
     }
-
-    protected override void Update()
+    public void Spawn()
     {
-        base.Update();
-
-    }
-    public void Spawn() {
         foreach (var enemy in enemyArray)
         {
             Instantiate(enemy).transform.position = this.transform.position;
         }
+    }
+    protected override void Update()
+    {
+        base.Update();
+/*        if (countdown <= 0f && waveInWave <= wave)         //freezuje więc zakomentowane
+        {
+            Spawn();
+            countdown = timeBetweenWaves;
+            waveInWave++;
+        }
+        countdown -= Time.deltaTime;
+
+        if (wave == waveInWave)
+        {
+            waveInWave = 0;
+            wave++;
+        }
+*/
     }
 
 }
