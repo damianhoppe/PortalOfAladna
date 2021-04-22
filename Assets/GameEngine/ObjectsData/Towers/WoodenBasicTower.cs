2a796c1684c3e3cd9a86a752b0f044ebc7994bfa
MKowalski11 <gasienica98@gmail.com>
2021-04-22 22:36:41 +0200

    Tower Collider + Wall Test
    Test of adding collider and building wall
    Forgotten commit

---
+++ Assets/GameEngine/ObjectsData/Towers/WoodenBasicTower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenBasicTower : DefaultBuilding
+{
+    CircleCollider2D AttackRange;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        this.AttackRange = this.GetComponent<CircleCollider2D>();
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+    WoodenBasicTower()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.gameObject.
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+
+        }
+    }
+}
