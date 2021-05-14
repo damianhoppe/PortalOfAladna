31125fec502ee970e9215111eca0d11639aeeeb9
MKowalski11 <gasienica98@gmail.com>
2021-05-14 12:08:34 +0200

    BePePe cleanup
    Cleaned code for test-enemy unit

--- Assets/GameEngine/ObjectsData/Enemies/biednaPierdolonaPiechota.cs
+++ Assets/GameEngine/ObjectsData/Enemies/biednaPierdolonaPiechota.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class biednaPierdolonaPiechota : defaultEnemy
 {
     [SerializeField]
-    public Vector3 MoveSpeed { get; protected set; }
+
     public Vector3 DestinationPosition { get; protected set; }
     public Vector3 StartPosition { get; protected set; }
-    public bool forward = true;
-    public bool back = false;
-
-    //public Position CurrentPosition { get; protected set; }
-    public Position[] trasa = { new Position(2, 0), new Position(2, 2), new Position(0, 2), new Position(0, 0) };
-    public int elementTrasy = 0;
-
-    public float moveSpeed = 10.0f;
     public Vector3 moveVector { get; protected set; }
 
+    public Position[] trasa = { new Position(2, 0), new Position(2, 2), new Position(0, 2), new Position(0, 0) };
+    public int elementTrasy = 0;
+    public float movePrecision = 0.2f;
+
+    public float moveSpeed = 10.0f;
+    protected override void Start()
+    {
+        this.moveSpeed = 0.0025f;
+        setDestination();
+        setPosition();
+        calculateVector();
+        base.Start();
+
+    }
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        this.transform.position += moveVector;
+        if (Vector3.Distance(this.transform.position, this.DestinationPosition) < movePrecision)
+        {
+            setDestination();
+            setPosition();
+            calculateVector();
+        }
+    }
     public void setPosition()
     {
         int x = Mathf.RoundToInt(this.transform.position.x);
@@ -30,8 +49,8 @@ public class biednaPierdolonaPiechota : defaultEnemy
     {
         Position target = trasa[elementTrasy];
         Vector3 punkt_docelowy = new Vector3((float)target.x, (float)target.y, -0.5f);
-        punkt_docelowy.x += Random.Range(-0.2f, 0.2f);
-        punkt_docelowy.y += Random.Range(-0.2f, 0.2f);
+        punkt_docelowy.x += Random.Range(-movePrecision, movePrecision);
+        punkt_docelowy.y += Random.Range(-movePrecision, movePrecision);
         this.DestinationPosition = punkt_docelowy;
     }
     public void calculateVector()
@@ -40,48 +59,7 @@ public class biednaPierdolonaPiechota : defaultEnemy
         float speedX = proporcja * (this.DestinationPosition.x - this.transform.position.x);
         float speedY = proporcja * (this.DestinationPosition.y - this.transform.position.y);
         this.moveVector = new Vector3(speedX, speedY, 0.0f);
-        //Debug.Log("MoveVector:"+moveVector);
 
-    }
-    protected override void Start()
-    {
-        this.moveSpeed = 0.0025f;
-        setDestination();
-        setPosition();
-        calculateVector();
-        //Debug.Log(this.transform.position);
-        //Debug.Log(this.DestinationPosition);
-        base.Start();
-        
-    }
-
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
-        /*
-        if (forward)
-        {
-            this.transform.position += MoveSpeed;
-            if (Vector3.Distance(this.transform.position, this.DestinationPosition) < 0.05f) forward = false;
-        }
-        else
-        {
-            this.transform.position -= MoveSpeed;
-            if (Vector3.Distance(this.transform.position, this.StartPosition) < 0.05f) forward = true;
-        }
-        */
-        this.transform.position += moveVector;
-        //Debug.Log(Vector3.Distance(this.transform.position, this.DestinationPosition));
-        if (Vector3.Distance(this.transform.position, this.DestinationPosition) < 0.2f)
-        {
-            
-            setDestination();
-            setPosition();
-            //Debug.Log(this.transform.position);
-            //Debug.Log(this.DestinationPosition);
-            calculateVector();
-        }
     }
     public void NextDestination()
     {
