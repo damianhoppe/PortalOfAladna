eadd08c523a7666108043a92ba8890548fa32666
MKowalski11 <gasienica98@gmail.com>
2021-05-14 02:54:08 +0200

    Be Pe Pe movement
    Be Pe Pe runs left and right
    Good for collider testing

--- Assets/GameEngine/ObjectsData/Enemies/biednaPierdolonaPiechota.cs
+++ Assets/GameEngine/ObjectsData/Enemies/biednaPierdolonaPiechota.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class biednaPierdolonaPiechota : defaultEnemy
 {
+    [SerializeField]
+    public Vector3 MoveSpeed { get; protected set; }
+    public Vector3 DestinationPosition { get; protected set; }
+    public Vector3 StartPosition { get; protected set; }
+    public bool forward = true;
+    public bool back = false;
     protected override void Start()
     {
+        StartPosition = this.transform.position;
+        DestinationPosition = this.transform.position+new Vector3(1.0f,0.0f,0.0f);
+        MoveSpeed= new Vector3(0.01f, 0.0f, 0.0f);
         base.Start();
+        
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+        if (forward)
+        {
+            this.transform.position += MoveSpeed;
+            if (Vector3.Distance(this.transform.position, this.DestinationPosition) < 0.05f) forward = false;
+        }
+        else
+        {
+            this.transform.position -= MoveSpeed;
+            if (Vector3.Distance(this.transform.position, this.StartPosition) < 0.05f) forward = true;
+        }
+        
+    }
+    public void NextDestination()
+    {
 
     }
+    public void Arrived()
+    {
 
+    }
 }
