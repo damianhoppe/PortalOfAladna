5309c4569d9cf71f586ba9f776158227af730074
TomaszKanzner <80108529+TomaszKanzner@users.noreply.github.com>
2021-05-19 00:20:06 +0200

    Spawners Prefabs
    -Dodano Prefaby spawnerów
    -Dodano skrypty do spawnerów
        *narazie podstawowe (byle by
          działało będzie rozbudowywane
           jak dojdą enemy)
        * Narazie Bez DayNight controlera w
          skrypcie (jak ogarne to dodam)

---
+++ Assets/GameEngine/BuildingSystem/Buildings/Scripts/Spawner_Med_Boss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_Med_Boss : Building
+{
+    public float timeBetweenWaves = 10f;
+    private float countdown = 2f;
+
+    [SerializeField]
+    GameObject[] enemyArray;
+    protected override void Start()
+    {
+        base.Start();
+    }
+    public void Spawn()
+    {
+        foreach (var enemy in enemyArray)
+        {
+            Instantiate(enemy).transform.position = this.transform.position;
+        }
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if (countdown <= 0f)
+        {
+            Spawn();
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+    }
+}
