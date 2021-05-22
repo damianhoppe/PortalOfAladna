24efd2b0334a6fcdbcf4068a24bbb7eb6ee518e4
MKowalski11 <gasienica98@gmail.com>
2021-05-23 01:57:28 +0200

    Gold Mines + Empty Mines scripts
    Added placeholder scripts for mines

--- Assets/GameEngine/ObjectsData/Mines/GoldDeepMine.cs
+++ Assets/GameEngine/ObjectsData/Mines/GoldDeepMine.cs
@@ -2,17 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldDeepMine : MonoBehaviour
+public class GoldDeepMine : DefaultMine
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
+
+        this.ObjectName = "Deep Gold Mine";
+        this.ObjectDescription = "This is a Deep Gold Mine.";
+        this.ObjectType = "Mine";
+        this.ObjectTypeID = -1;
+        this.ObjectSubtype = "Default Mine";
+        this.ObjectSubtypeID = -1;
+
+        this.MaxHitpoints = 250.0f;
+        this.ActiveAtNight = false;
+        this.Armor = 3.0f;
+        this.Protection = 5.0f;
+
+        this.PositionValue = 15.0f;
+        this.PositionObstacle = 3.0f;
+
+        this.BaseCost = new DataStructures.Cost(250.0f, 100.0f, 50.0f, 25.0f, 0.0f, 0.0f);
+        this.EnergyToBuild = 25.0f;
+        this.RequiredHumans = 20;
+
+        this.PlayerObjectID = 32;
+
+        this.MiningPower = 2.0f;
+        this.MiningSpeed = 1.0f;
+        this.MinedResource = "Gold";
+        this.MinesDeep = true;
+
+        this.CurrentHitpoints = this.MaxHitpoints;
+
+        this.RequiresInventor = true;
+        this.RequiresResearcher = false;
+        this.RequiresAcademy = false;
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+
+    }
+
+    GoldDeepMine()
+    {
+        this.ObjectName = "Deep Gold Mine";
+        this.ObjectDescription = "This is a Deep Gold Mine.";
+        this.ObjectType = "Mine";
+        this.ObjectTypeID = -1;
+        this.ObjectSubtype = "Default Mine";
+        this.ObjectSubtypeID = -1;
+
+        this.MaxHitpoints = 250.0f;
+        this.ActiveAtNight = false;
+        this.Armor = 3.0f;
+        this.Protection = 5.0f;
+
+        this.PositionValue = 15.0f;
+        this.PositionObstacle = 3.0f;
+
+        this.BaseCost = new DataStructures.Cost(250.0f, 100.0f, 50.0f, 25.0f, 0.0f, 0.0f);
+        this.EnergyToBuild = 25.0f;
+        this.RequiredHumans = 20;
+
+        this.PlayerObjectID = 32;
+
+        this.MiningPower = 2.0f;
+        this.MiningSpeed = 1.0f;
+        this.MinedResource = "Gold";
+        this.MinesDeep = true;
+
+        this.CurrentHitpoints = this.MaxHitpoints;
     }
 }
--- Assets/GameEngine/ObjectsData/Mines/GoldMine.cs
+++ Assets/GameEngine/ObjectsData/Mines/GoldMine.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldMine : MonoBehaviour
+public class GoldMine : DefaultMine
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
+
+        this.ObjectName = "Small Gold Mine";
+        this.ObjectDescription = "This is a Small Gold Mine.";
+        this.ObjectType = "Mine";
+        this.ObjectTypeID = -1;
+        this.ObjectSubtype = "Default Mine";
+        this.ObjectSubtypeID = -1;
+
+        this.MaxHitpoints = 50.0f;
+        this.ActiveAtNight = false;
+        this.Armor = 1.0f;
+        this.Protection = 0.0f;
+
+        this.PositionValue = 5.0f;
+        this.PositionObstacle = 1.0f;
+
+        this.BaseCost = new DataStructures.Cost(0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+        this.EnergyToBuild = 10.0f;
+        this.RequiredHumans = 5;
+
+        this.PlayerObjectID = 31;
+
+        this.MiningPower = 1.0f;
+        this.MiningSpeed = 1.0f;
+        this.MinedResource = "Gold";
+        this.MinesDeep = false;
+
+        this.CurrentHitpoints = this.MaxHitpoints;
+
+        this.RequiresInventor = false;
+        this.RequiresResearcher = false;
+        this.RequiresAcademy = false;
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+    }
+
+    GoldMine()
+    {
+        this.ObjectName = "Small Gold Mine";
+        this.ObjectDescription = "This is a Small Gold Mine.";
+        this.ObjectType = "Mine";
+        this.ObjectTypeID = -1;
+        this.ObjectSubtype = "Default Mine";
+        this.ObjectSubtypeID = -1;
+
+        this.MaxHitpoints = 50.0f;
+        this.ActiveAtNight = false;
+        this.Armor = 1.0f;
+        this.Protection = 0.0f;
+
+        this.PositionValue = 5.0f;
+        this.PositionObstacle = 1.0f;
+
+        this.BaseCost = new DataStructures.Cost(0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+        this.EnergyToBuild = 10.0f;
+        this.RequiredHumans = 5;
+
+        this.PlayerObjectID = 31;
+
+        this.MiningPower = 1.0f;
+        this.MiningSpeed = 1.0f;
+        this.MinedResource = "Gold";
+        this.MinesDeep = false;
+
+        this.CurrentHitpoints = this.MaxHitpoints;
     }
 }
