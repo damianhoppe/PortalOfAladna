e3a6e8bff54fd3212dba24cee533e2a694adb051
MKowalski11 <gasienica98@gmail.com>
2021-05-23 00:38:36 +0200

    Registering mechanics, controllers for MC
    Mining Controllers knows DayNightController and EconomyController, knows its mines

--- Assets/GameEngine/ObjectsData/tymczasowe/MiningController.cs
+++ Assets/GameEngine/ObjectsData/tymczasowe/MiningController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class MiningController : MonoBehaviour
 {
+    List<DefaultMine> RegisteredMines = new List<DefaultMine>();
+    public virtual EconomyController EC { get; protected set; }
+    public virtual DayNightController DNC { get; protected set; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EC = GameObject.Find("PlayerDataController").GetComponent<EconomyController>();
+        DNC = GameObject.Find("PlayerDataController").GetComponent<DayNightController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public bool RegisterMine(DefaultMine Mine)
+    {
+        if (this.RegisteredMines.Contains(Mine))
+        {
+            return false;
+        }
+        else
+        {
+            this.RegisteredMines.Add(Mine);
+            return true;
+        }
+    }
+    public bool UnregisterMine(DefaultMine Mine)
+    {
+        if (this.RegisteredMines.Contains(Mine))
+        {
+            this.RegisteredMines.Remove(Mine);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
