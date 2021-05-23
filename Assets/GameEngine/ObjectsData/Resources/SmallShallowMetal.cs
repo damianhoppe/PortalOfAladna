3049b9b1e9e8a457975e601ae1113db584aec590
MKowalski11 <gasienica98@gmail.com>
2021-05-23 03:21:33 +0200

    Small Shallow Mines
    Added Small Shallow Mines

---
+++ Assets/GameEngine/ObjectsData/Resources/SmallShallowMetal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallShallowMetal : DefaultResource
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        this.ObjectName = "Small Shallow Metal";
+        this.ObjectDescription = "This is a Small Shallow Metal.";
+
+        this.OreRichness = 0.5f;
+        this.OreType = "Metal";
+        this.LimitOre = 500;
+        this.RemainingOre = this.LimitOre;
+
+        this.Infinite = false;
+        this.Depleted = false;
+        this.Deep = false;
+
+        this.PlayerObjectID = 50;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+    SmallShallowMetal()
+    {
+        this.ObjectName = "Small Shallow Metal";
+        this.ObjectDescription = "This is a Small Shallow Metal.";
+
+        this.OreRichness = 0.5f;
+        this.OreType = "Metal";
+        this.LimitOre = 500;
+        this.RemainingOre = this.LimitOre;
+
+        this.Infinite = false;
+        this.Depleted = false;
+        this.Deep = false;
+
+        this.PlayerObjectID = 50;
+    }
+}
---
+++ Assets/GameEngine/ObjectsData/Resources/SmallShallowWood.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallShallowWood : DefaultResource
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        this.ObjectName = "Small Shallow Wood";
+        this.ObjectDescription = "This is a Small Shallow Wood.";
+
+        this.OreRichness = 0.5f;
+        this.OreType = "Wood";
+        this.LimitOre = 500;
+        this.RemainingOre = this.LimitOre;
+
+        this.Infinite = false;
+        this.Depleted = false;
+        this.Deep = false;
+
+        this.PlayerObjectID = 44;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+    SmallShallowWood()
+    {
+        this.ObjectName = "Small Shallow Wood";
+        this.ObjectDescription = "This is a Small Shallow Wood.";
+
+        this.OreRichness = 0.5f;
+        this.OreType = "Wood";
+        this.LimitOre = 500;
+        this.RemainingOre = this.LimitOre;
+
+        this.Infinite = false;
+        this.Depleted = false;
+        this.Deep = false;
+
+        this.PlayerObjectID = 44;
+    }
+}
